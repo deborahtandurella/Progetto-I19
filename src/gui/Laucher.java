965c9b5e86758d511f5c5da42a398c85b1b0b9a5
frahferra <francesco.ferrara03@universitadipavia.it>
2019-05-28 15:07:25 +0200

    Aggiunto metodo per fill ScrollPane in ViasualizzaProdotti

--- src/gui/Laucher.java
+++ src/gui/Laucher.java
@@ -1,10 +1,15 @@
 package gui;
 
+import eccezioni.PrezzoNegativoException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import prodotti.Prodotto;
+import prodotti.TipoProdotto;
+
+import java.util.ArrayList;
 
 public class Laucher extends Application {
 
@@ -19,5 +24,29 @@ public class Laucher extends Application {
         primaryStage.setTitle("Ristorante");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
+    }
+
+    /*
+    Creazione prodotti per testare la grafica
+     */
+    static Prodotto p1, p2, p3;
+
+    static {
+        try {
+            p1 = new Prodotto("Pasta Carbonara",10,"Pasta alla carbonara con guanciale", 10, TipoProdotto.CUCINA);
+            p2 = new Prodotto("Risotto alla Milanese",12,"Il segreto di questo piatto della tradizione milanese risiede nella semplicità dei suoi ingredienti, che accostati tra loro creano un sapore raffinato e setoso.",10, TipoProdotto.CUCINA);
+            p3 = new Prodotto("carne",18,"carne rossa", 15,TipoProdotto.CUCINA);
+
+        } catch (PrezzoNegativoException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static ArrayList retProdotti(){
+        ArrayList<Prodotto> list = new ArrayList();
+        list.add(p1);
+        list.add(p2);
+        list.add(p3);
+        return list;
     }
 }
--- src/gui/VisualizzaProdottiController.java
+++ src/gui/VisualizzaProdottiController.java
@@ -1,30 +1,59 @@
 package gui;
 
+import com.jfoenix.controls.JFXButton;
 import gui.utils.Clock;
 import gui.utils.FXMLManager;
 import javafx.event.ActionEvent;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Label;
-import javafx.stage.Stage;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import prodotti.Prodotto;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 public class VisualizzaProdottiController implements Initializable {
 
     public Label time;
+    public VBox vBoxList;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         Clock.initClock(time);
+        fillList();
     }
 
     public void loadHome(ActionEvent event) throws IOException {
         FXMLManager.loadFXML(event, "/gui/Home.fxml");
+    }
+
+    public void fillList(){
+        ArrayList<Prodotto> lista = Laucher.retProdotti();
+
+        for(Prodotto p : lista){
+
+            AnchorPane tempPane = new AnchorPane();
+            JFXButton addTemp = new JFXButton("+");
+            Text titleTemp = new Text(p.getNome());
+            titleTemp.setId("titletemp");
+            Text descTemp = new Text(p.getDescrizione());
+            descTemp.setId("desctemp");
+
+            tempPane.getChildren().addAll(titleTemp, descTemp, addTemp);
+            tempPane.getStylesheets().add(getClass().getResource("/gui/style/Style.css").toExternalForm());
+            addTemp.setLayoutX(562.0);
+            addTemp.setLayoutY(6.0);
+            titleTemp.setLayoutX(7.0);
+            titleTemp.setLayoutY(22.0);
+            descTemp.setLayoutX(7.0);
+            descTemp.setLayoutY(40.0);
+            descTemp.setWrappingWidth(562.0);
+
+            vBoxList.getChildren().addAll(tempPane);
+        }
     }
 }
