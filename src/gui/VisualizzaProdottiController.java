f60787574df373a33b3c56d921892d84d2561d26
daniel <daniel.belcore01@universitadipavia.it>
2019-05-29 21:41:24 +0200

    Inizio modifiche per incrementare bottore ordini

--- src/gui/VisualizzaProdottiController.java
+++ src/gui/VisualizzaProdottiController.java
@@ -1,12 +1,16 @@
 package gui;
 
+import com.jfoenix.controls.JFXButton;
 import gui.utils.Clock;
 import gui.utils.FXMLManager;
 import gui.utils.ListFiller;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
+import prodotti.TipoPortata;
 
 import java.io.IOException;
 import java.net.URL;
@@ -16,7 +20,7 @@ public class VisualizzaProdottiController implements Initializable {
 
     public Label time;
     public VBox vBoxList;
-
+    private int n=0;
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         Clock.initClock(time);
@@ -42,5 +46,13 @@ public class VisualizzaProdottiController implements Initializable {
 
     public void loadHome(ActionEvent event) throws IOException {
         FXMLManager.loadFXML(event, "/gui/Home.fxml");
+    }
+    public void aggiornaConteggioOrdine(ActionEvent event)
+    {
+
+       JFXButton ButtonConteggio=new JFXButton();
+       ButtonConteggio.setText(Integer.toString(n));
+       n++;
+        System.out.println(n);
     }
 }
