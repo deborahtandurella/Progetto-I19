c03ac5fb5ecac636eac58d2a68975424ebc0c4f0
frahferra <francesco.ferrara03@universitadipavia.it>
2019-06-02 23:07:12 +0200

    Aggiunto attributo ID ai prodotti per testare gui

--- src/prodotti/Prodotto.java
+++ src/prodotti/Prodotto.java
@@ -3,12 +3,14 @@ package prodotti;
 import eccezioni.PrezzoNegativoException;
 
 public class Prodotto {
+    private final int id;
     private final String nome;
     private final float prezzo;
     private final String descrizione;
     private int tempoPreparazione;
     private TipoProdotto tipo;
     private TipoPortata tipoPortata;
+    private static int newId = 0;
 
     public Prodotto(String nome,float prezzo, String descrizione, int tempoPreparazione, TipoProdotto tipo, TipoPortata tipoP) throws PrezzoNegativoException {
     	
@@ -21,6 +23,7 @@ public class Prodotto {
         this.tempoPreparazione = tempoPreparazione;
         this.tipo = tipo;
         this.tipoPortata = tipoP;
+        this.id = newId++;
     }
 
     public float getPrezzo() {
@@ -35,6 +38,10 @@ public class Prodotto {
 
     public int getTempoPreparazione() {
         return tempoPreparazione;
+    }
+
+    public int getId() {
+        return id;
     }
 
     @Override
