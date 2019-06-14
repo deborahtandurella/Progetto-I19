f2c629f5f2bc597d68beb9a7a55f6a3c55977fc3
Nicolò Ramaioli <nicolo.ramaioli@gmail.com>
2019-06-14 11:17:17 +0200

    Modifiche per POST Resquest usando la chiave esterna

--- i19_backend/backend/serializer.py
+++ i19_backend/backend/serializer.py
@@ -1,48 +1,56 @@
-from rest_framework.serializers import ModelSerializer, IntegerField, DateTimeField
+from rest_framework import serializers
 from .models import *
 
 
-class TipoProdottoSerializer(ModelSerializer):
+class TipoProdottoSerializer(serializers.ModelSerializer):
     class Meta:
         model = TipoPortata
         fields = ['id', 'nome']
 
 
-class TipoPortataSerializer(ModelSerializer):
+class TipoPortataSerializer(serializers.ModelSerializer):
     class Meta:
         model = TipoPortata
         fields = ['id', 'nome']
 
 
-class ProdottoSerializer(ModelSerializer):
-    tipo = IntegerField(source='tipo_prodotto.id')
-    tipoPortata = IntegerField(source='tipo_portata.id')
+class ProdottoSerializer(serializers.ModelSerializer):
+    tipo = serializers.IntegerField(source='tipo_prodotto.id')
+    tipoPortata = serializers.IntegerField(source='tipo_portata.id')
 
     class Meta:
         model = Prodotto
         fields = ['id', 'descrizione', 'prezzo', 'tempo_preparazione', 'tipo', 'tipoPortata']
 
 
-class StatoProdottoOrdinatoSerializer(ModelSerializer):
+class StatoProdottoOrdinatoSerializer(serializers.ModelSerializer):
      class Meta:
          model = StatoProdottoOrdinato
          fields = ['id', 'nome']
 
 
-class ProdottoOrdinatoSerializer(ModelSerializer):
-    prodotto = ProdottoSerializer()
-    stato = IntegerField(source='stato_prodotto_ordinato.id')
-    tempoInizioLavorazione = DateTimeField(format="%Y-%m-%d %H:%M:%S")
+class ProdottoOrdinatoSerializer(serializers.ModelSerializer):
+    prodotto = serializers.PrimaryKeyRelatedField(queryset=Prodotto.objects.all())
+    stato = serializers.IntegerField(source='stato_prodotto_ordinato.id')
+    tempoInizioLavorazione = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S")
 
     class Meta:
         model = ProdottoOrdinato
         fields = ['id', 'quantita', 'tempoInizioLavorazione', 'prodotto', 'stato']
 
+    def to_representation(self, instance):
+        self.fields['prodotto'] = ProdottoSerializer()
+        return super(ProdottoOrdinatoSerializer, self).to_representation(instance)
 
-class OrdinazioneSerializer(ModelSerializer):
-    prodotto_ordinato = ProdottoOrdinatoSerializer()
+
+class OrdinazioneSerializer(serializers.ModelSerializer):
+    prodotto_ordinato = serializers.PrimaryKeyRelatedField(queryset=ProdottoOrdinato.objects.all())
 
     class Meta:
         model = Ordinazione
         fields = ['id', 'id_tavolo', 'prodotto_ordinato']
+
+    def to_representation(self, instance):
+        self.fields['prodotto_ordinato'] = ProdottoOrdinatoSerializer()
+        return super(OrdinazioneSerializer, self).to_representation(instance)
 
