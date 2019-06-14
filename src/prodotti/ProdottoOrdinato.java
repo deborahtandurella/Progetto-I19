8b04b403566fe608e2eacfb37a11ecff81030173
Nicolò Ramaioli <nicolo.ramaioli@gmail.com>
2019-06-14 09:55:44 +0200

    Aggiunta notations il parsing delle date

--- src/prodotti/ProdottoOrdinato.java
+++ src/prodotti/ProdottoOrdinato.java
@@ -5,17 +5,30 @@ import eccezioni.OrdinazioneNegativaException;
 import java.time.Duration;
 import java.time.LocalDateTime;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class ProdottoOrdinato {
 	private final Prodotto prodotto;
 	private int quantita;
 	private StatoProdottoOrdinato stato;
+	
+	@DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
 	private LocalDateTime tempoInizioLavorazione;
 
+	public ProdottoOrdinato() {
+		this.prodotto = null;
+	}
+	
 	public ProdottoOrdinato(Prodotto prodotto, int quantita) throws OrdinazioneNegativaException {
 		if (quantita <= 0) {
 			throw new OrdinazioneNegativaException();
 		}
-
+		
 		this.prodotto = prodotto;
 		this.quantita = quantita;
 		this.stato = null;
