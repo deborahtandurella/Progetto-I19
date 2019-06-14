e0ec6f59e30c2dee6dff46583a60f99d2177e7a7
Nicolò Ramaioli <nicolo.ramaioli@gmail.com>
2019-06-14 18:54:01 +0200

    Aggiunto file contente gli indirizzi per le REST API

---
+++ src/serverCentrale/ApiURL.java
@@ -0,0 +1,10 @@
+package serverCentrale;
+
+public final class ApiURL {
+	
+	public static final String BASE_NAME = "http://localhost:8000/";
+	public static final String PRODOTTO = BASE_NAME + "prodotto/";
+	public static final String PRODOTTO_ORDINATO = BASE_NAME + "prodotto_ordinato/";
+	public static final String ORDINAZIONE = BASE_NAME + "ordinazione/";
+	
+}
