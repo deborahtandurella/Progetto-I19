14bf536b8b2933bcdc1dcf6a977195d14fe14296
Nicolò Ramaioli <nicolo.ramaioli@gmail.com>
2019-06-24 09:53:55 +0200

    Rimosso print per log.

--- i19_backend/backend/views.py
+++ i19_backend/backend/views.py
@@ -34,7 +34,6 @@ class ProdottoOrdinatoViewSet(viewsets.ModelViewSet):
         else:
             serializer = self.get_serializer(data=request.data, many=True)
             serializer.is_valid(raise_exception=True)
-            print(request.data)
             self.perform_create(serializer)
             headers = self.get_success_headers(serializer.data)
             return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
