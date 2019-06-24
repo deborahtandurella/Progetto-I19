32962221b6156af80d0ad8380842e024c3c62370
Nicolò Ramaioli <nicolo.ramaioli@gmail.com>
2019-06-24 09:59:42 +0200

    Aggiunto ProdottoOrdinatoSerializer per Spring.

---
+++ src/prodotti/ProdottoOrdinatoSerializer.java
@@ -0,0 +1,39 @@
+package prodotti;
+
+import java.io.IOException;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
+
+public class ProdottoOrdinatoSerializer extends StdSerializer<ProdottoOrdinato>{
+
+	public ProdottoOrdinatoSerializer() {
+		this(ProdottoOrdinato.class);
+	}
+	
+	public ProdottoOrdinatoSerializer(Class<ProdottoOrdinato> t) {
+		super(t);
+	}
+	
+	@Override
+	public void serialize(ProdottoOrdinato value, JsonGenerator gen, SerializerProvider provider) throws IOException {
+		gen.writeStartObject();
+		
+		gen.writeNumberField("idTavolo", value.getIdTavolo());
+		gen.writeNumberField("quantita", value.getQuantita());
+		
+		if(value.getTempoInizioLavorazione() == null) {
+			gen.writeStringField("tempoInizioLavorazione", null);
+		}else {
+			gen.writeStringField("tempoInizioLavorazione", value.getTempoInizioLavorazione().toString());
+		}
+		
+		gen.writeNumberField("prodotto", value.getProdotto().getId());
+		gen.writeNumberField("statoProdottoOrdinato", value.getStato().value());
+
+		gen.writeEndObject();
+		
+	}
+
+}
