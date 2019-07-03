cde48b46d6bd5c426774de05a5883570a80ff1ab
Nicolò Ramaioli <nicolo.ramaioli01@universitadipavia.it>
2019-07-03 10:15:00 +0200

    Aggiunto indirizzo della VPS

--- src/serverCentrale/ApiURL.java
+++ src/serverCentrale/ApiURL.java
@@ -2,7 +2,8 @@ package serverCentrale;
 
 public final class ApiURL {
 	
-	public static final String BASE_NAME = "http://localhost:8000/";
+	//public static final String BASE_NAME = "http://localhost:8000/";
+	public static final String BASE_NAME = "http://137.74.165.12/";
 	public static final String PRODOTTO = BASE_NAME + "prodotto/";
 	public static final String PRODOTTO_ORDINATO = BASE_NAME + "prodotto_ordinato/";
 	public static final String ORDINAZIONE = BASE_NAME + "ordinazione/";
