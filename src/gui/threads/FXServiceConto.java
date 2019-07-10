5f795eb4f200da7638fded3988e82998d89dd958
MoroniFabio <fabio.moroni01@universitadipavia.it>
2019-07-10 12:51:41 +0200

    - implementazione getConto() + Thread + aggiunte mancanti commit precedente

---
+++ src/gui/threads/FXServiceConto.java
@@ -0,0 +1,27 @@
+package gui.threads;
+
+import javafx.concurrent.Service;
+import javafx.concurrent.Task;
+import serverCentrale.ServerCentraleEsterno;
+
+public class FXServiceConto extends Service {
+
+    private ServerCentraleEsterno serverCentraleEsterno;
+    private int idTavolo;
+
+    public FXServiceConto(ServerCentraleEsterno serverCentraleEsterno, int idTavolo) {
+        this.serverCentraleEsterno = serverCentraleEsterno;
+        this.idTavolo = idTavolo;
+    }
+
+    @Override
+    protected Task<Float> createTask() {
+        return new Task<Float>() {
+            @Override
+            protected Float call() throws Exception {
+                System.out.println("---------------"+Float.valueOf(serverCentraleEsterno.getConto(idTavolo)));
+                return Float.valueOf(serverCentraleEsterno.getConto(idTavolo));
+            }
+        };
+    }
+}
