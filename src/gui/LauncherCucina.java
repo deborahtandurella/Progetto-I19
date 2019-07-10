895f500b811ceb220fd8b26e27512bf7c4dea6f4
MoroniFabio <fabio.moroni01@universitadipavia.it>
2019-07-10 12:50:30 +0200

    - implementazione getConto() + Thread

--- src/gui/LauncherCucina.java
+++ src/gui/LauncherCucina.java
@@ -15,7 +15,6 @@ public class LauncherCucina extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("Cucina.fxml"));
-
         primaryStage.setTitle("Cucina");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
