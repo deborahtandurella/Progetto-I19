b383c40c09dc9c659b6fb4eff23ec058114ede92
root <root@vps705885.ovh.net>
2019-07-14 11:52:28 +0200

    Modifica / Aggiunta file di configurazione per database Mysql

--- i19_backend/i19_backend/settings.py
+++ i19_backend/i19_backend/settings.py
@@ -76,9 +76,15 @@ WSGI_APPLICATION = 'i19_backend.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': '/root/Progetto-I19/i19_backend/i19_backend/client_mysql.cnf',
+        },
     }
 }
 
