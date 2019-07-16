bc9f76a3e440ee91a20fb7ab90a01721f8e2e9be
Nicolò Ramaioli <nicolo.ramaioli01@universitadipavia.it>
2019-07-17 00:47:45 +0200

    Rinominati i Prodotti nel test.

--- src/test/ServerCentraleInternoTest.java
+++ src/test/ServerCentraleInternoTest.java
@@ -29,10 +29,10 @@ class ServerCentraleInternoTest {
 
         ArrayList<Prodotto> ordine = new ArrayList<>();
 
-        Prodotto p1 = new Prodotto(1, "Acqua tranqui", (float) 2.0, "Prodotto interessante la caffetteria.", 0 , TipoProdotto.CAFFETTERIA, TipoPortata.BEVANDE);
-        Prodotto p2 = new Prodotto(2, "Carbonara", (float) 12.0, "descrizione carbonara", 12 , TipoProdotto.CUCINA, TipoPortata.PIATTI);
+        Prodotto p1 = new Prodotto(1, "Acqua tranqui", (float) 2.0, "Prodotto interessante della caffetteria.", 0 , TipoProdotto.CAFFETTERIA, TipoPortata.BEVANDE);
+        Prodotto p2 = new Prodotto(2, "Carbonara", (float) 12.0, "Descrizione carbonara", 12 , TipoProdotto.CUCINA, TipoPortata.PIATTI);
         Prodotto p3 = new Prodotto(3, "Barolo", (float) 35.0, "Il Barolo si presenta di colore rosso granato con riflessi aranciati. Al naso è complesso, persistente ed intenso. A note fruttate e floreaii (viola, vaniglia) si accompagnano note più speziate e di goudron.", 0 , TipoProdotto.CAFFETTERIA, TipoPortata.VINI);
-        Prodotto p4 = new Prodotto(4, "Tiramisù", (float) 8.0, "descrizione dolce", 10 , TipoProdotto.CUCINA, TipoPortata.DOLCI);
+        Prodotto p4 = new Prodotto(4, "Tiramisù", (float) 8.0, "Descrizione dolce", 10 , TipoProdotto.CUCINA, TipoPortata.DOLCI);
         ordine.add(p2);
 
         assertArrayEquals(ordine.toArray(), s.getOrdini(TipoProdotto.CUCINA).toArray());
