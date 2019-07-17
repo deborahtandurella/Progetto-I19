1b3d4a4939e409657e73df4666f8cf542cab8de1
frahferra <francesco.ferrara03@universitadipavia.it>
2019-07-17 13:38:12 +0200

    Modifica grafica setIdTavolo

--- src/gui/cliente/controller/TableIdController.java
+++ src/gui/cliente/controller/TableIdController.java
@@ -5,30 +5,44 @@ import com.jfoenix.controls.JFXTextField;
 import eccezioni.ErrorTableException;
 import gui.cliente.utils.FXMLManager;
 import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class TableIdController {
+public class TableIdController implements Initializable {
 
     public JFXTextField textTavolo;
     public static Integer idTavolo;
-    public Pane pannello;
     public JFXButton submit;
+    public Pane pannello;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        pannello.getStylesheets().add(getClass().getResource("/gui/cliente/style/StyleTavolo.css").toExternalForm());
+    }
+
+    private void errorMessage(){
+        textTavolo.clear();
+        textTavolo.setPromptText("Inserisci solo valori numerici");
+    }
 
     public void getIdTavolo(ActionEvent event) throws IOException {
         submit.setId("submit");
         textTavolo.setId("tavolo");
-        //submit.getStylesheets().add("/gui/cliente/style/StyleTavolo.css");
-        submit.getStylesheets().add(getClass().getResource("/gui/cliente/style/StyleTavolo.css").toExternalForm());
+
         try {
             idTavolo = Integer.valueOf(textTavolo.getText());
             FXMLManager.loadFXML(event, "/gui/cliente/resources/Home.fxml");
         }catch (NumberFormatException e )
         {
-            FXMLManager.loadFXML(event, "/gui/cliente/resources/SetId.fxml");
+            this.errorMessage();
         }
 
     }
+
+
 }
